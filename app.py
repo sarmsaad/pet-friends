0ed3c6d1e8e1aa232062bd96a2401eb126e4302f
sarmsaad <sarah.msaad@yahoo.com>
2018-01-27 12:48:57 -0500

    flask

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, abort, jsonify, Response
+
+import requests
+
+url = "https://language.googleapis.com/v1/documents:analyzeSentiment?key=" + config.GAPI
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.json:
+        abort(400)
+    if authenticate(request):
+        return jsonify({'login': 'successful'}), 201
+    else:
+        return jsonify({'login': 'fail'}), 201
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    if not request.json:
+        abort(400)
+    if storedata(request):
+        return jsonify({'signup': 'successful'}), 201
+    else:
+        return jsonify({'signup': 'fail'}), 201
+
+@app.route('/journal', methods=['POST'])
+def saveJounal():
+
+    text = request.json['text']
+    """
+    Pass the text through Google Cloud NLP and get the sentiment of the text
+    Note: We only take English.
+    :param text: The text to analyse
+    :return:
+    """
+    document = {
+        'type': 'PLAIN_TEXT',
+        'language': 'en',
+        'content': text,
+    }
+    data = {
+        'document': document,
+        'encodingType': 'utf8'
+    }
+    r = requests.post(url=url, json=data)
+    if r.status_code == 200:
+        read = r.json()
+        return read
+    elif r.status_code == 429:
+        time.sleep(1)
+        return None
+
+def authenticate(request):
+    #the function to authenticate the login
+
+def storedata(request):
+    #the function that's going to store the new patient
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,6 @@
+"""
+config file 
+"""
+
+#google api key
+GAPI = "AIzaSyD1q_EryIdBeAgA8u1tvE5Fd6RvmRFSYDI"
