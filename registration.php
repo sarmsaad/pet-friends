cb658a7ed7aa481a35920daf7f73ae0e1b74e00f
sarmsaad <sarah.msaad@yahoo.com>
2018-01-27 21:33:57 -0500

    registration connection

--- registration.php
+++ registration.php
@@ -23,10 +23,15 @@ Website: http://www.allphptricks.com/
 		$password = ($_REQUEST['password']);
 		$password = ($con,$password);
 
+		//send a request
+        $url = 'http://127.0.0.1:5000/signup?username' . $username . '&password=' . $password . '&location=' . $email;
+        $json = @file_get_contents($url);
+        $obj = json_decode($json);
+        $bool = $obj->{'signup'};
+
 		$trn_date = date("Y-m-d H:i:s");
         $query = "INSERT into `users` (username, password, email, trn_date) VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
-        $result = 
-        if($result){
+        if($bool = "successful"){
             echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
         }
     }else{
